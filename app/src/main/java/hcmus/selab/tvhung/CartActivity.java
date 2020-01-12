02dc71d79be6a5dff78fd86a397a6d0425be3663
tranvinhhung99 <xing@cinnamon.is>
2020-01-12 12:14:56 +0700

    add item to cart function

---
+++ app/src/main/java/hcmus/selab/tvhung/CartActivity.java
@@ -0,0 +1,22 @@
+package hcmus.selab.tvhung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+
+    private int queryTotalPrice(){
+
+        return 0;
+    }
+}
